0471dbe90fc81f42ba999a476b1d371ceaf75bc2
caffeinedriven1337 <98424072+caffeinedriven1337@users.noreply.github.com>
2022-03-18 09:41:37 -0400

    Added Win page code.

--- app/src/main/java/com/example/project/ActiveMain.java
+++ app/src/main/java/com/example/project/ActiveMain.java
@@ -1,4 +1,22 @@
 package com.example.project;
 
-public class ActiveMain {
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActiveMain extends AppCompatActivity {
+
+
+
+    public void add_score(){
+        ImageButton button = findViewById(R.id.add_scorebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/project/WinPage.java
+++ app/src/main/java/com/example/project/WinPage.java
@@ -3,6 +3,7 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WinPage extends AppCompatActivity {
 
@@ -11,4 +12,10 @@ public class WinPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_page);
     }
+
+    public void win_score(){
+        TextView score_display = findViewById(R.id.score_display);
+        score_display.setText("1");
+    }
+
 }
