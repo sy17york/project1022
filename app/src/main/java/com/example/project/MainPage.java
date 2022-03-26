b3473fd6b60b5b0b957f3458c6eb30a11c92f7be
alanw13 <99999181+alanw13@users.noreply.github.com>
2022-03-25 23:33:21 -0400

    3/18/22 9:39

--- app/src/main/java/com/example/project/MainPage.java
+++ app/src/main/java/com/example/project/MainPage.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 public class MainPage extends AppCompatActivity {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
