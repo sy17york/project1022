43ffae40fc72b3d160ab0b7bca8276d86af2ec57
alanw13 <99999181+alanw13@users.noreply.github.com>
2022-03-28 01:41:17 -0400

    1:37 3/28/22

--- app/src/main/java/com/example/project/MainPage.java
+++ app/src/main/java/com/example/project/MainPage.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
 
 public class MainPage extends AppCompatActivity {
     Button E_button;
@@ -16,6 +20,9 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        String city = "London,UK";
+        MainPage.JSONWeatherTask task = new MainPage.JSONWeatherTask();
+        task.execute(new String[]{city});
         E_button = findViewById(R.id.button3);
         E_button.setOnClickListener(new View.OnClickListener() {
                                         @Override
@@ -54,6 +61,41 @@ public class MainPage extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ( (new WeatherHTTP()).getWeatherData(params[0]));
+            try {
+                weather = JSONparser.getWeather(data);
+                weather.iconData = ( (new WeatherHTTP()).getImage(weather.currentCondition.getIcon()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+        @Override
+        protected void onPostExecute(Weather weather) {
+            TextView cityText = (TextView) findViewById(R.id.cityText);
+            TextView condDescr = (TextView) findViewById(R.id.condDescr);
+            TextView temp = (TextView) findViewById(R.id.temp);
+            TextView hum = (TextView) findViewById(R.id.hum);
+            TextView press = (TextView) findViewById(R.id.press);
+            TextView windSpeed = (TextView) findViewById(R.id.windSpeed);
+            TextView windDeg = (TextView) findViewById(R.id.windDeg);
+            super.onPostExecute(weather);
+            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+            temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
+            hum.setText("" + weather.currentCondition.getHumidity() + "%");
+            press.setText("" + weather.currentCondition.getPressure() + " hPa");
+            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+            windDeg.setText("" + weather.wind.getDeg() + "�");
+
+        }
     }
 
 }
