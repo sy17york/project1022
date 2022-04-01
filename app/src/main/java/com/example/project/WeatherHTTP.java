45e07297660e13f74ee22ef4f29ada2c87df8402
alanw13 <99999181+alanw13@users.noreply.github.com>
2022-04-01 17:30:01 -0400

    4/1/2022 5:27

--- app/src/main/java/com/example/project/WeatherHTTP.java
+++ app/src/main/java/com/example/project/WeatherHTTP.java
@@ -8,61 +8,37 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class WeatherHTTP {
-
     private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-    private static String IMG_URL = "http://openweathermap.org/img/w/";
+    private static String APPID = "{13ea03b9a160703f897b5d992b6600e5}";
     public String getWeatherData(String location) {
-        HttpURLConnection con = null ;
+        HttpURLConnection con = null;
         InputStream is = null;
         try {
-            con = (HttpURLConnection) ( new URL(BASE_URL + location)).openConnection();
+            con = (HttpURLConnection) (new URL(BASE_URL + location + "&APPID=" + APPID)).openConnection();
             con.setRequestMethod("GET");
             con.setDoInput(true);
             con.setDoOutput(true);
             con.connect();
-            StringBuffer buffer = new StringBuffer();
+            StringBuilder buffer = new StringBuilder();
             is = con.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             String line = null;
-            while (  (line = br.readLine()) != null )
+            while ((line = br.readLine()) != null)
                 buffer.append(line + "\r\n");
             is.close();
             con.disconnect();
             return buffer.toString();
-        }
-        catch(Throwable t) {
+        } catch (Throwable t) {
             t.printStackTrace();
-        }
-        finally {
-            try { is.close(); } catch(Throwable t) {}
-            try { con.disconnect(); } catch(Throwable t) {}
-        }
-        return null;
-    }
-    public byte[] getImage(String code) {
-        HttpURLConnection con = null ;
-        InputStream is = null;
-        try {
-            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
-            con.setRequestMethod("GET");
-            con.setDoInput(true);
-            con.setDoOutput(true);
-            con.connect();
-            is = con.getInputStream();
-            byte[] buffer = new byte[1024];
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            while ( is.read(buffer) != -1)
-                baos.write(buffer);
-
-            return baos.toByteArray();
-        }
-        catch(Throwable t) {
-            t.printStackTrace();
-        }
-        finally {
-            try { is.close(); } catch(Throwable t) {}
-            try { con.disconnect(); } catch(Throwable t) {}
+        } finally {
+            try {
+                is.close();
+            } catch (Throwable t) {
+            }
+            try {
+                con.disconnect();
+            } catch (Throwable t) {
+            }
         }
         return null;
     }
