aa869f8902e713c0318aa3105de3d7e89055f151
caffeinedriven1337 <98424072+caffeinedriven1337@users.noreply.github.com>
2022-04-02 00:39:15 -0400

    Changes

--- app/src/main/java/com/example/project/StoreData.java
+++ app/src/main/java/com/example/project/StoreData.java
@@ -3,9 +3,9 @@ package com.example.project;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-
+//This class uses SharedPreferences to Store Score Data
 public class StoreData{
-
+    //putScores and getScores accepts, stores and return scores as array
     public void putScores(Context context, String[]array, String arrayName){
         SharedPreferences prefs = context.getSharedPreferences("TopScores", 0);
         SharedPreferences.Editor editor = prefs.edit();
@@ -22,7 +22,7 @@ public class StoreData{
             array[i] = prefs.getString(arrayName + "_" + i, null);
         return array;
     }
-
+    //putNames and getNames accepts, stores and return names as array
     public void putNames(Context context, String[]array, String arrayName){
         SharedPreferences prefs = context.getSharedPreferences("TopNames", 0);
         SharedPreferences.Editor editor = prefs.edit();
